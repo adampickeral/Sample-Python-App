b7124c85b01478c7e06f94a4e39935dba1240820
adampickeral <adam.pickeral@rackspace.com>
2011-12-12 22:29:40 -0500

    added message wall http server

---
+++ message_wall01.py
@@ -0,0 +1,13 @@
+from wsgiref.simple_server import make_server
+
+def message_wall_app(environ, start_response):
+    status = b'200 OK'
+    headers = [(b'Content-type', b'text/html; charset=utf-8')]
+    start_response(status, headers)
+
+    return["<h1>Message Wall</h1>"]
+
+httpd = make_server('', 8000, message_wall_app)
+print("Serving on port 8000...")
+
+httpd.server_forever()
